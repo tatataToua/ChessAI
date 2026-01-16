4cbd34530d1f25cb42e5db938f94663e7d9fb647
Toua Lee <toualee10@gmail.com>
2026-01-16 11:02:53 -0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center py-2">
       <main className="m-auto flex w-full max-w-2xl flex-col items-center justify-center p-4">
+        <h1>Chess Coach AI</h1>
         <Chess></Chess>
       </main>
     </div>
